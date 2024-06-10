a5de3c99492e6a9e7a22f8e74452c850f72969da
BuildTools <unconfigured@null.spigotmc.org>
2024-06-10 22:59:54 +0200

    Added: - test for hiring new Employee

--- src/test/java/nsk/Employee/TestHiringToCompany.java
+++ src/test/java/nsk/Employee/TestHiringToCompany.java
@@ -1,4 +1,31 @@
 package src.test.java.nsk.Employee;
 
+import org.junit.Before;
+import org.junit.Test;
+import src.main.java.nsk.Application;
+import src.main.java.nsk.Company.Company;
+import src.main.java.nsk.Company.Position;
+import src.main.java.nsk.Employee.Entity;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestHiringToCompany {
+
+    private Application app;
+
+    @Before
+    public void init() {
+        app = new Application();
+        app.addCompany( new Company("Val", "Gdańsk", "123-456-789") );
+    }
+
+    @Test
+    public void testHiringToCompany() {
+        Entity e = new Entity("Gson", 298, "Unknown");
+
+        Company v = app.getCompany("Val");
+        v.hireEmployee(e, new Position("HR Manager", 3750));
+
+        assertEquals(3750, v.getEmployee(0).getPosition().getSalary(), 0.01 );
+    }
 }
